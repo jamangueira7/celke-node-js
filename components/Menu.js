b3a2210dcbc175ca2fb830b2ce1853b59a7732ff
João Mangueira <jamjs7@gmail.com>
2021-03-02 19:17:27 -0300

    add menu

--- components/Menu.js
+++ components/Menu.js
@@ -14,11 +14,45 @@ import {
     NavbarText
 } from 'reactstrap';
 
-function Menu() {
+const Menu = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
         <div>
-            <a href="/">Home</a><br />
-            <a href="/orcamento">Orçamento</a><br />
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Option 1
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Option 2
+                                </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                    Reset
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                    <NavbarText>Simple Text</NavbarText>
+                </Collapse>
+            </Navbar>
         </div>
     );
 }
